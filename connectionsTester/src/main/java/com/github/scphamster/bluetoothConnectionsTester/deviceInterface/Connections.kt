4b7e9a10004c1c9e9ca1a8e8e7f8d139642ea740
scpha <scp.hamster@gmail.com>
2023-01-21 13:08:56 +0100

    classes reposition

--- connectionsTester/src/main/java/com/github/scphamster/bluetoothConnectionsTester/deviceInterface/Connections.kt
+++ connectionsTester/src/main/java/com/github/scphamster/bluetoothConnectionsTester/deviceInterface/Connections.kt
@@ -76,23 +76,6 @@ class Voltage(override val value: CircuitParamT, override val precision: Int = 2
     }
 }
 
-interface PinIdentifier {
-    /**
-     * @brief composes affinity and id into pattern like affinity:id example: 37:1
-     */
-    fun getPrettyName(): String
-    public val pinAffinityAndId: PinAffinityAndId
-}
-
-data class PinAffinityAndId(val boardId: IoBoardIndexT, val idxOnBoard: PinNumT) : PinIdentifier {
-    override fun getPrettyName(): String {
-        return "$boardId:$idxOnBoard"
-    }
-
-    override val pinAffinityAndId: PinAffinityAndId
-        get() = PinAffinityAndId(boardId, idxOnBoard)
-}
-
 class Connection(val toPin: PinIdentifier,
                  val voltage: Voltage? = null,
                  val resistance: Resistance? = null,
@@ -125,44 +108,6 @@ class Connection(val toPin: PinIdentifier,
         }
 
         return false
-    }
-}
-
-data class PinDescriptor(val affinityAndId: PinAffinityAndId,
-                         val UID: PinNumT? = null,
-                         var name: String? = null,
-                         var group: PinGroup? = null) : PinIdentifier {
-    override fun getPrettyName(): String {
-        val string_builder = StringBuilder()
-
-        if (group != null) {
-            string_builder.append(group?.getPrettyName())
-        }
-        else {
-            string_builder.append(affinityAndId.boardId.toString())
-        }
-
-        string_builder.append(":")
-
-        if (name != null) {
-            string_builder.append(name)
-        }
-        else {
-            string_builder.append(affinityAndId.idxOnBoard.toString())
-        }
-
-        return string_builder.toString()
-    }
-
-    override val pinAffinityAndId: PinAffinityAndId
-        get() = affinityAndId
-
-    fun clearPinAndGroupNames() {
-        group?.let {
-            group = PinGroup(it.id, null)
-        }
-
-        name = null
     }
 }
 
--- connectionsTester/src/main/java/com/github/scphamster/bluetoothConnectionsTester/deviceInterface/Pin.kt
+++ connectionsTester/src/main/java/com/github/scphamster/bluetoothConnectionsTester/deviceInterface/Pin.kt
@@ -2,6 +2,62 @@ package com.github.scphamster.bluetoothConnectionsTester.deviceInterface
 
 import java.lang.ref.WeakReference
 
+interface PinIdentifier {
+    /**
+     * @brief composes affinity and id into pattern like affinity:id example: 37:1
+     */
+    fun getPrettyName(): String
+    public val pinAffinityAndId: PinAffinityAndId
+}
+
+data class PinAffinityAndId(val boardId: IoBoardIndexT, val idxOnBoard: PinNumT) : PinIdentifier {
+    override fun getPrettyName(): String {
+        return "$boardId:$idxOnBoard"
+    }
+
+    override val pinAffinityAndId: PinAffinityAndId
+        get() = PinAffinityAndId(boardId, idxOnBoard)
+}
+
+data class PinDescriptor(val affinityAndId: PinAffinityAndId,
+                         val UID: PinNumT? = null,
+                         var name: String? = null,
+                         var group: PinGroup? = null) : PinIdentifier {
+    override fun getPrettyName(): String {
+        val string_builder = StringBuilder()
+
+        if (group != null) {
+            string_builder.append(group?.getPrettyName())
+        }
+        else {
+            string_builder.append(affinityAndId.boardId.toString())
+        }
+
+        string_builder.append(":")
+
+        if (name != null) {
+            string_builder.append(name)
+        }
+        else {
+            string_builder.append(affinityAndId.idxOnBoard.toString())
+        }
+
+        return string_builder.toString()
+    }
+
+    override val pinAffinityAndId: PinAffinityAndId
+        get() = affinityAndId
+
+    fun clearPinAndGroupNames() {
+        group?.let {
+            group = PinGroup(it.id, null)
+        }
+
+        name = null
+    }
+}
+
+
 class Pin(val descriptor: PinDescriptor,
           var belongsToBoard: WeakReference<IoBoard> = WeakReference<IoBoard>(null),
           var connectionsListChangedFromPreviousCheck: Boolean = false) {
